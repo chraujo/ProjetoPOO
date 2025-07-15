9556bd40e1bcedb647a1d6cd74e03e25d5fb65e4
chris araujo <141365858+chraujo@users.noreply.github.com>
2025-07-15 15:34:01 -0300

    Adição da logica de busca de midias na View Home

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+import BaseDeMidiaController from "./Controllers/BaseDeMidiaController.js";
+import BaseDeUsuarioController from "./Controllers/BaseDeUsuarioController.js";
+class LeadingPage {
+    constructor() {
+        let baseMidia = new BaseDeMidiaController();
+        let baseUsuario = new BaseDeUsuarioController();
+    }
+}
+document.addEventListener('DOMContentLoaded', () => {
+    new LeadingPage();
+});
+//# sourceMappingURL=index.js.map
